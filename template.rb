22309c31db7f506183ca17f43d049f1f9e541f77
darwin christopher tantuco <darwinxciii@yahoo.com>
2019-01-16 15:23:07 +0800

    Improve version checking logic, allow future versions

--- template.rb
+++ template.rb
@@ -1,19 +1,26 @@
 require 'fileutils'
 require 'shellwords'
 
-def rails_version
-  run 'rails -v'
+def version(version)
+  Gem::Version.create(version)
+end
+
+def version_met?(current, expected)
+  (version(current) <=> version(expected)) >= 0
 end
 
 def check_ruby_version
   # Refactor,  get from .tool-versions instead'
-  # Improve checking logic, allow future versions lol
-  # allow ruby 2.7.x
-  # allow rails 5.3.x
-  minimum_ruby_version = '2.6'
-  minimum_rails_version = '5.2'
-  abort("Aborted! Required ruby version: > #{minimum_ruby_version}") unless RUBY_VERSION.to_s.include? minimum_ruby_version
-  abort("Aborted! Required rails version: > #{minimum_rails_version}") unless rails_version.to_s.include? minimum_rails_version
+  minimum_ruby_version = '2.6.0'
+  minimum_rails_version = '5.2.0'
+
+  unless version_met?(RUBY_VERSION.to_s, minimum_ruby_version)
+    abort("Aborted! Required ruby version >=#{minimum_ruby_version}.")
+  end
+
+  unless version_met?(Rails.version.to_s, minimum_rails_version)
+    abort("Aborted! Required rails version >=#{minimum_rails_version}")
+  end
 end
 
 # Copied from https://github.com/mattbrictson/rails-template
