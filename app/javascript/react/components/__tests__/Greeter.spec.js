207a5de02f7924995a1fb5697a388cdd09345329
darwin christopher tantuco <darwinxciii@yahoo.com>
2019-03-25 01:38:10 +0800

    Add snapshot test

--- app/javascript/react/components/__tests__/Greeter.spec.js
+++ app/javascript/react/components/__tests__/Greeter.spec.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import renderer from 'react-test-renderer'
+
 import Greeter from '../Greeter'
 
 it('renders without crashing', () => {
   shallow(<Greeter />)
+})
+
+it('matches the snapshot', () => {
+  const tree = renderer.create(<Greeter name='lodi' />).toJSON()
+
+  expect(tree).toMatchSnapshot()
 })
